3f82c2c645df8418e15ce7c5b10fe29136f7b148
ninhtrong2k <ninhtrongchannel@gmail.com>
2024-03-27 01:14:13 +0700

    b176 Tổng quan về Laravel Event - Tạo Event và List trong Laravel

---
+++ app/Listeners/SendEmailAfterOderPayment.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\OrderPayment;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SendEmailAfterOderPayment
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \App\Events\OrderPayment  $event
+     * @return void
+     */
+    public function handle(OrderPayment $event)
+    {
+        //
+        return false;
+    }
+}
